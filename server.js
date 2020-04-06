7c4d2b510ed22fc84a848bbbcd0b2cf6ddc6b366
zillahpoliks8 <poliquitzillahit1b@gmail.com>
2020-04-06 11:38:06 +0800

    new files 060420

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/bna.html'));
+});
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
